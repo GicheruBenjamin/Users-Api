8f6a75fa2dbff674e271ea555c5082967401fe21
GicheruBenjamin <benjaminnjuguna77@gmail.com>
2025-01-22 11:39:49 +0300

    feature :Development start.

---
+++ src/app.mjs
@@ -0,0 +1,16 @@
+import express from 'express';
+//simple endpoint
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
